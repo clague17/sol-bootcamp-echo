e5321512a68043f8bbc3825c24f786650dbf0c49
clague <lclague9@gmail.com>
2022-01-12 10:46:38 -0600

    almost done with initializeEcho

--- js/index.js
+++ js/index.js
@@ -16,16 +16,16 @@ const main = async () => {
   const echo = args[1];
 
   const connection = new Connection("http://127.0.0.1:8899");
+  // const connection = new Connection("https://api.devnet.solana.com");
 
   const feePayer = new Keypair();
   const echoBuffer = new Keypair();
 
   // for Instruction 1
 
-  const authorizedBuffer = new Keypair();
-
   console.log("Requesting Airdrop of 1 SOL...");
-  await connection.requestAirdrop(feePayer.publicKey, 2e9);
+  let placeholder = await connection.requestAirdrop(feePayer.publicKey, 2e9);
+  await connection.confirmTransaction(placeholder);
   console.log("Airdrop received");
 
   let createIx = SystemProgram.createAccount({
@@ -34,7 +34,7 @@ const main = async () => {
     /** Amount of lamports to transfer to the created account */
     lamports: await connection.getMinimumBalanceForRentExemption(echo.length),
     /** Amount of space in bytes to allocate to the created account */
-    space: echo.length,
+    space: echo.length + 9, // account for padding in initializeAuthorizedEcho
     /** Public key of the program to assign as the owner of the created account */
     programId: programId,
   });
@@ -58,45 +58,54 @@ const main = async () => {
   });
 
   const authBuffSize = Buffer.from(
-    new Uint8Array(new BN(echo.length).toArray("le", 9))
+    new Uint8Array(new BN(echo.length + 9).toArray("le", 8))
   );
 
-  let programBuff = Buffer.from(
-    new Uint8Array(["authority", feePayer.publicKey, authBuffSize])
+  const buffSeed = 10;
+  // random hardcoded to 10
+  const authBuffSeed = Buffer.from(
+    new Uint8Array(new BN(buffSeed).toArray("le", 8))
   );
 
-  let(auth_key, bump_seed) = PublicKey.findProgramAddress(
-    programBuff,
+  let programBuff = Buffer.from(new Uint8Array(["authority"]));
+
+  let [auth_key, bump_seed] = await PublicKey.findProgramAddress(
+    [programBuff, feePayer.publicKey.toBuffer(), authBuffSeed],
     programId
   );
 
+  console.log("Auth_key: ", auth_key);
+  console.log("feepayer public key: ", feePayer.publicKey);
+  console.log("Buffer seed: ", authBuffSeed);
+
   let initializeBufferIx = new TransactionInstruction({
     keys: [
-      { pubkey: authorizedBuffer.publicKey, isSigner: false, isWritable: true },
+      { pubkey: auth_key, isSigner: false, isWritable: true },
       { pubkey: feePayer.publicKey, isSigner: true, isWriteable: false }, // The authority here can be anyone! in this case I'm just initializing it to feePayer.
       { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
     ],
     programId: programId,
-    data: Buffer.concat([idx, messageLen, authBuffSize]),
+    data: Buffer.concat([idx, authBuffSeed, authBuffSize]),
   });
 
   let tx = new Transaction();
-  tx.add(createIx).add(echoIx);
   tx.add(initializeBufferIx);
 
-  let txid = await sendAndConfirmTransaction(
-    connection,
-    tx,
-    [feePayer, echoBuffer],
-    {
-      skipPreflight: true,
-      preflightCommitment: "confirmed",
-      confirmation: "confirmed",
-    }
+  console.log(
+    "Before sendAndConfirmTransaction: ",
+    await connection.getBalance(feePayer.publicKey)
   );
+
+  let txid = await sendAndConfirmTransaction(connection, tx, [feePayer], {
+    skipPreflight: true,
+    preflightCommitment: "confirmed",
+    confirmation: "confirmed",
+    commitment: "confirmed",
+  });
   console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`);
 
-  data = (await connection.getAccountInfo(echoBuffer.publicKey)).data;
+  data = (await connection.getAccountInfo(echoBuffer.publicKey, "confirmed"))
+    .data;
   console.log("Echo Buffer Text:", data.toString());
 };
 
